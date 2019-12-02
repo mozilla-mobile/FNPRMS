88ff7fac9068eb736b8fc82d60e9c57956ad765f
Will Hawkins <whh8b@obs.cr>
2019-12-02 10:11:14 -0500

    Update to a new APK URL location

--- run.sh
+++ run.sh
@@ -24,7 +24,7 @@ function run {
 
 homeactivity_start_command='am start-activity org.mozilla.fenix.nightly/org.mozilla.fenix.HomeActivity'
 applink_start_command='am start-activity -t "text/html" -d "about:blank" -a android.intent.action.VIEW org.mozilla.fenix.nightly/org.mozilla.fenix.IntentReceiverActivity'
-apk_url_template="https://index.taskcluster.net/v1/task/project.mobile.fenix.v2.nightly.DATE.latest/artifacts/public/build/armeabi-v7a/geckoNightly/target.apk"
+apk_url_template="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.mobile.fenix.v2.nightly.DATE.latest/artifacts/public/build/armeabi-v7a/geckoNightly/target.apk"
 log_dir=/home/hawkinsw/run_logs/
 test_date=`date +"%Y.%m.%-d"`
 log_base=${test_date}
