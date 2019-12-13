7db87f7036ee338822194d6f90a8d044532bd341
Will Hawkins <whh8b@obs.cr>
2019-12-13 02:22:47 -0500

    Update path to al.plot files.

--- plot.sh
+++ plot.sh
@@ -9,16 +9,15 @@ cd ${iamhere}
 
 log_dir=/home/hawkinsw/run_logs/
 
-
 cwd=`pwd`
 cd ${log_dir}
 
-gnuplot ${iwashere}/${iamhere}/ha.plot > /dev/null 2>&1
-gnuplot ${iwashere}/${iamhere}/al.plot > /dev/null 2>&1
+gnuplot ${iamhere}/ha.plot 
+gnuplot ${iamhere}/al.plot
 
-#git add *.png
-#git commit -m "${log_base} Graph the results."
-#git push fenix-mobile master -q
+git add *.png
+git commit -m "${log_base} Graph the results."
+git push fenix-mobile master -q
 cd ${cwd}
 
 cd ${iwashere}
