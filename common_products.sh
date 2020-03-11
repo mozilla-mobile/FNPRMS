4c658acffab7cb01adeda55eba78228d040cf5d3
Michael Comella <michael.l.comella@gmail.com>
2020-03-11 16:40:52 -0700

    For #69: unify page loaded for app link.
    This should not have affected our G5 nightly numbers because we're only
    running tests on the fennec-nightly variants.

--- common_products.sh
+++ common_products.sh
@@ -9,14 +9,16 @@
 # product strings: fennec, fennec-nightly, fenix-nightly, fenix-performance
 PRODUCTID=$1
 
+APPLINK_URL="https://example.com"
+
 # Define "parameters" needed to execute all the tests. These names should
 # be self explanatory. See README.md for customization information.
 fenix_homeactivity_start_command='am start-activity org.mozilla.fenix.nightly/org.mozilla.fenix.HomeActivity'
-fenix_applink_start_command='am start-activity -t "text/html" -d "about:blank" -a android.intent.action.VIEW org.mozilla.fenix.nightly/org.mozilla.fenix.IntentReceiverActivity'
+fenix_applink_start_command="am start-activity -t 'text/html' -d "$APPLINK_URL" -a android.intent.action.VIEW org.mozilla.fenix.nightly/org.mozilla.fenix.IntentReceiverActivity"
 fenix_url_template="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.mobile.fenix.v2.nightly.DATE.latest/artifacts/public/build/armeabi-v7a/geckoNightly/target.apk"
 
 fennec_homeactivity_start_command='am start-activity org.mozilla.fennec_aurora/org.mozilla.fenix.HomeActivity'
-fennec_applink_start_command='am start-activity -t "text/html" -d "https://example.com" -a android.intent.action.VIEW org.mozilla.fennec_aurora/org.mozilla.fenix.IntentReceiverActivity '
+fennec_applink_start_command="am start-activity -t 'text/html' -d "$APPLINK_URL" -a android.intent.action.VIEW org.mozilla.fennec_aurora/org.mozilla.fenix.IntentReceiverActivity"
 fennec_url_template="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.mobile.fenix.v2.fennec-nightly.DATE.latest/artifacts/public/build/arm64-v8a/geckoNightly/target.apk"
 
 fennec_url_template_g5="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.mobile.fenix.v2.fennec-nightly.2020.02.24.latest/artifacts/public/build/armeabi-v7a/geckoNightly/target.apk"
